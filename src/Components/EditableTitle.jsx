f71006e2143a7b7908b482d53bd39b1b7abbbd0c
Liam Kennedy <liamkennedy89@hotmail.com>
2019-01-27 20:39:39 +0000

    updated react to alpha version to try hooks

--- src/Components/EditableTitle.jsx
+++ src/Components/EditableTitle.jsx
@@ -1,48 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
-class EditableTitle extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { editMode: false };
-  }
-  render() {
-    return (
-      <div>
-        {this.state.editMode && (
-          <form
-            className="pure-form animated fadeIn"
-            onSubmit={e => {
-              e.preventDefault();
-              this.setState({ editMode: false });
-            }}
-          >
-            <input
-              autoFocus
-              type="text"
-              value={this.props.value}
-              onChange={this.props.onChange}
-              onBlur={() => this.setState({ editMode: false })}
-            />
-            <button type="submit" className="pure-button u-margin-left">
-              Save
+function EditableTitle({value, onChange}) {
+  const [editMode, setEditMode] = useState(false);
+  return (
+    <div>
+      {editMode && (
+        <form
+          className="pure-form animated fadeIn"
+          onSubmit={e => {
+            e.preventDefault();
+            setEditMode(false);
+          }}
+        >
+          <input
+            autoFocus
+            type="text"
+            value={value}
+            onChange={onChange}
+            onBlur={() => setEditMode(false)}
+          />
+          <button type="submit" className="pure-button u-margin-left">
+            Save
             </button>
-          </form>
-        )}
-        {!this.state.editMode && (
-          <span
-            className="headline"
-            onClick={() => this.setState({ editMode: true })}
-            style={{ cursor: "pointer" }}
-          >
-            <h1 onClick={() => this.setState({ editMode: true })}>
-              {this.props.value}
-            </h1>
-            <small>click to edit</small>
-          </span>
-        )}
-      </div>
-    );
-  }
+        </form>
+      )}
+      {!editMode && (
+        <span
+          className="headline"
+          onClick={() => setEditMode(true)}
+          style={{ cursor: "pointer" }}
+        >
+          <h1 onClick={() => setEditMode(true)}>
+            {value}
+          </h1>
+          <small>click to edit</small>
+        </span>
+      )}
+    </div>
+  );
 }
 
 export default EditableTitle;
